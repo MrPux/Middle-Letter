9cf50f5fb220f899fdf2170a0be48d73fa959af8
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-22 13:30:02 -0500

    First commit with instructions

---
+++ Middle_Letter.py
@@ -0,0 +1,8 @@
+#Write a function named "mid" that takes a string as its parameter. Your function should extract and return the middle letter.
+# If there is no middle letter, your function should return the empty string
+
+#For example, mid("abc") should return "b" and mid("aaaa") should return "".
+
+def mid(string):
+    mid = [i for i in list(string)]
+    return mid
